615602d65dcfbcfd09fa9c0ffd01cd24462993b9
kakiuchi@LAPTOP <kakiuchi@honeycomb-lab.co.jp>
2024-06-18 10:55:09 +0900

    Cubeを掴めたり、表示/非表示の切り替えができるように

---
+++ SpectatorViewTest-Hololens 2/Assets/Scripts/CubeActiveToggle.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeActiveToggle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void CubeToggle()
+    {
+        if (gameObject.activeSelf)
+        {
+            gameObject.SetActive(false);
+        }
+        else
+        {
+            gameObject.SetActive(true);
+        }
+    }
+}
